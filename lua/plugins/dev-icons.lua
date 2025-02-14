62b7cab70a419231a65903582a7ce0546a136d4e
Vijay Senapathi <vijaysenapathi@gmail.com>
2025-02-14 22:14:53 +0530

    fixed devicons not loading

--- lua/plugins/dev-icons.lua
+++ lua/plugins/dev-icons.lua
@@ -5,11 +5,10 @@
 return {
   "nvim-tree/nvim-web-devicons", name = "web-devicons",
   enabled = vim.g.nerd_font_enabled,
-  config = function ()
-    require("nvim-web-devicons").setup({
-      color_icons = true
-    })
-    vim.opts.listchars = { tab = ' ', trail = '󱁐', nbsp = '󰍴' }
-  end
+  config = true
+  -- config = function ()
+  --   require("nvim-web-devicons").setup {}
+  --   vim.opts.listchars = { tab = ' ', trail = '󱁐', nbsp = '󰍴' }
+  -- end
 }
 
