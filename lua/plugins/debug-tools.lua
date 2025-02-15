8f759db29cd43ea94c5a9817f4e01a85edbc732b
Vijay Senapathi <vijaysenapathi@gmail.com>
2025-02-15 14:07:38 +0530

    moved debug tools to github sources

--- lua/plugins/debug-tools.lua
+++ lua/plugins/debug-tools.lua
@@ -1,9 +1,9 @@
 -- tools for debugging during plugin development
---
+-- source: https://github.com/vijaysenapathi/nvim-debug-tools
 
 return {
-  dir = "~/Projects/Personal/nvim-debug-tools",
-  name = "debug-tools",
+  "vijaysenapathi/nvim-debug-tools",
+  name = "debug-tools", enable = false,
   config = function ()
     require("debug-tools").setup({})
   end
